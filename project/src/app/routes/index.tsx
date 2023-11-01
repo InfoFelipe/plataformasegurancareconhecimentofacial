1b9e69bde4223ccd116658011c471f4d6b5d1e65
Felipe Antonio <96073664+InfoFelipe@users.noreply.github.com>
2023-11-01 10:29:56 -0300

    Feat ThemeColors
    Configuração do tema de cores global para a aplicação.

--- project/src/app/routes/index.tsx
+++ project/src/app/routes/index.tsx
@@ -1,18 +1,22 @@
 
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import { Register } from "../pages"
+import { ThemeProvider } from "@emotion/react"
+import { LightTheme } from "../shared/themes"
 
 
 export const Rotas = () => {
 
     return(
         <>
+        <ThemeProvider theme={LightTheme}>
         <BrowserRouter>
             <Routes>
                 <Route   path="/page-register" element={<Register/>}/>
                 <Route   path="*" element={<Navigate to="/page-register"/>}/>
             </Routes>
         </BrowserRouter>
+        </ThemeProvider>
         </>
     )
 }
