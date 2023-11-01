13a8f048f671373f6bcc50614cc644a9a7a323af
Felipe Antonio <96073664+InfoFelipe@users.noreply.github.com>
2023-11-01 12:00:04 -0300

    Themes
    Configuração de Themes Light e Dark

--- project/src/app/App.tsx
+++ project/src/app/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import { Rotas } from "./routes";
+import { AppThemeProvider } from "./shared/contexts";
 
 
 export const App = () => {
   return (
     <>
-    <Rotas/>
+      <AppThemeProvider>
+      <BrowserRouter>
+        <Rotas/>
+      </BrowserRouter>
+      </AppThemeProvider>
     </>
   );
 }
